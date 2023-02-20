ceb926abfc1c31261cba7e5f1c8a9449911e1581
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-20 17:49:39 +0100

    creata api controller e fatta route

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,10 @@ Route::post('/movie/edit/{movie}', [MainController::class, 'movieUpdate'])
 
 Route::get('/movie/delete/{movie}', [MainController::class, 'movieDelete'])
     ->name('movie.delete');
+
+
+// questo va in route/api
+
+use App\Http\Controllers\ApiController;
+
+Route::get('/api/v1/movie/all', [ApiController::class, 'movieAll']);
